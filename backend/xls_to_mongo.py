baa6288d71d8e069ec15657c2daf8585a9203554
Dmitri <warstream55@gmail.com>
2022-03-20 22:26:33 +0300

    Конвертирование из Экселя в Монго

---
+++ backend/xls_to_mongo.py
@@ -0,0 +1,26 @@
+import openpyxl as xls
+import pymongo
+import json
+
+with open('static/datafile.json') as f:
+    datavars = json.load(f)
+
+wb = xls.load_workbook(datavars['workbook'])
+ws = wb[datavars['worksheet']]
+con = pymongo.MongoClient(host='localhost', port=27017)
+db = con.db
+collection = db.trashers
+r = []
+for row in ws.iter_rows(min_row=2):
+    d = {
+        'address': row[0].value,
+        'coords': row[1].value
+    }
+    c = []
+    for i in range(3, 16):
+        if row[i].value == 1:
+            c.append(i - 2)
+    d['types'] = c
+    r.append(pymongo.InsertOne(d))
+res = collection.bulk_write(r)
+con.close()
