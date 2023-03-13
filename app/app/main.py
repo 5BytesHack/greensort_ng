5f0df925cc1b361a83d16aec06c4a471fc5b4a32
Dmitri <warstream55@gmail.com>
2023-03-13 21:33:38 +0300

    cors?????

--- app/app/main.py
+++ app/app/main.py
@@ -4,15 +4,12 @@ from .greensort.routes import api_router
 
 app = FastAPI()
 
-origins = [
-    '*'
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*']
 )
+
 app.include_router(api_router)
